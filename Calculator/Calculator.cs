d92c0738dc69f2dd767f7da2dbcec8698b243724
Konstantin Shved <2006SKA@gmail.com>
2025-04-06 17:20:45 +0300

    Добавлены классы Калькулятора и ВидМоделиКалькулятора. Добавлены методы взаимодействия в главной форме

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Calculator
+    {
+        private double result;
+        private string operation;
+
+        public double Result => result;
+
+        public void SetOperation(string operation)
+        {
+            this.operation = operation;
+        }
+
+        public void PerformOperation(double number)
+        {
+            switch (operation)
+            {
+                case "+":
+                    result += number;
+                    break;
+                case "-":
+                    result -= number;
+                    break;
+                case "*":
+                    result *= number;
+                    break;
+                case "/":
+                    if (number != 0)
+                        result /= number;
+                    else
+                        throw new DivideByZeroException("Деление на ноль!");
+                    break;
+                default:
+                    result = number;
+                    break;
+            }
+        }
+
+        public void Clear()
+        {
+            result = 0;
+            operation = string.Empty;
+        }
+    }
+}
---
+++ Calculator/CalculatorViewModel.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class CalculatorViewModel
+    {
+        private Calculator calculator;
+        private string display;
+        private string auxiliryDisplay;
+
+        public string Display
+        {
+            get => display;
+            set
+            {
+                display = value;
+            }
+        }
+
+        public string AuxiliaryDisplay
+        {
+            get => auxiliryDisplay;
+            set
+            {
+                auxiliryDisplay = value;
+            }
+        }
+
+        public CalculatorViewModel()
+        {
+            calculator = new Calculator();
+            Display = "0";
+            AuxiliaryDisplay = "";
+        }
+
+        public void NumberButtonClick(string number)
+        {
+            if (Display == "0")
+                Display = number;
+            else
+                Display += number;
+        }
+
+        public void OperationButtonClick(string operation)
+        {
+            double number = double.Parse(Display);
+            calculator.PerformOperation(number);
+            calculator.SetOperation(operation);
+            Display = "0";
+            AuxiliaryDisplay = number.ToString() + operation;
+        }
+
+        public void EqualsClick()
+        {
+            double number = double.Parse(Display);
+            calculator.PerformOperation(number);
+            Display = calculator.Result.ToString();
+            AuxiliaryDisplay += number.ToString() + "=";
+        }
+
+        public void Clear() // Метод для AC
+        {
+            calculator.Clear();
+            Display = "0";
+            AuxiliaryDisplay = "";
+        }
+
+        public void Del() // Метод для DEL
+        {
+            if (Display.Length > 1)
+            {
+                Display = Display.Substring(0, Display.Length - 1);
+            }
+            else
+            {
+                Display = "0"; // Если у нас остается пустая строка, устанавливаем "0"
+            }
+        }
+    }
+}
